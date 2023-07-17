da42b0f646972ff840697301e2aeb4000303c788
myak <myakushek@outlook.com>
2023-07-17 11:44:47 +0300

    refactor: преттиер

--- src/javascripts/map.js
+++ src/javascripts/map.js
@@ -1,44 +1,46 @@
 window.map = null;
 
-if (location.pathname === '/map') {
-    main();
+if (location.pathname === "/map") {
+  main();
 }
 
 document.addEventListener("location-change", () => {
-    const map = document.getElementById('map');
-    if (map) {
-        main();
-    }
+  const map = document.getElementById("map");
+  if (map) {
+    main();
+  }
 });
 
 async function main() {
-    await ymaps3.ready;
-    const {
-        YMap,
-        YMapDefaultSchemeLayer,
-        YMapMarker,
-        YMapControls,
-        YMapDefaultFeaturesLayer
-    } = ymaps3;
+  await ymaps3.ready;
+  const {
+    YMap,
+    YMapDefaultSchemeLayer,
+    YMapMarker,
+    YMapControls,
+    YMapDefaultFeaturesLayer
+  } = ymaps3;
 
-    const {YMapZoomControl} = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
-    const {YMapDefaultMarker} = await ymaps3.import('@yandex/ymaps3-markers@0.0.1');
+  const { YMapZoomControl } = await ymaps3.import(
+    "@yandex/ymaps3-controls@0.0.1"
+  );
+  const { YMapDefaultMarker } = await ymaps3.import(
+    "@yandex/ymaps3-markers@0.0.1"
+  );
 
-    const LOCATION = {center: [34.100323, 44.948246], zoom: 8};
+  const LOCATION = { center: [34.100323, 44.948246], zoom: 8 };
 
-    map = new YMap(document.getElementById('map'), {location: LOCATION}, [
-        new YMapDefaultSchemeLayer(),
-        new YMapControls({position: 'right'}, [
-            new YMapZoomControl({})
-        ]),
-    ]);
+  map = new YMap(document.getElementById("map"), { location: LOCATION }, [
+    new YMapDefaultSchemeLayer(),
+    new YMapControls({ position: "right" }, [new YMapZoomControl({})])
+  ]);
 
-    map.addChild(new YMapDefaultFeaturesLayer({id: 'features'}));
+  map.addChild(new YMapDefaultFeaturesLayer({ id: "features" }));
 
-    map.addChild(new YMapDefaultMarker({
-        coordinates: [34.100323, 44.948246],
-        title: 'Дом, здорово',
-    }));
-
-
+  map.addChild(
+    new YMapDefaultMarker({
+      coordinates: [34.100323, 44.948246],
+      title: "Дом, здорово"
+    })
+  );
 }
--- src/javascripts/router.js
+++ src/javascripts/router.js
@@ -1,39 +1,41 @@
-import {locationChangeEvent} from './events/event.js'
+import { locationChangeEvent } from "./events/event.js";
 
 const routesPath = "/static/views";
 
 const routes = {
-    error: "",
-    "/activity": `${routesPath}/activity.html`,
-    "/map": `${routesPath}/map.html`,
-    "/timer": `${routesPath}/timer.html`
+  error: "",
+  "/activity": `${routesPath}/activity.html`,
+  "/map": `${routesPath}/map.html`,
+  "/timer": `${routesPath}/timer.html`
 };
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    switchLocation();
-}
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  switchLocation();
+};
 
 async function switchLocation() {
-    const path = window.location.pathname;
+  const path = window.location.pathname;
 
-    if (path === '/') {
-        document.getElementById("content").innerHTML = '';
-        return;
-    }
+  if (path === "/") {
+    document.getElementById("content").innerHTML = "";
+    return;
+  }
 
-    const route = routes[path] || routes[error];
+  const route = routes[path] || routes[error];
 
-    document.getElementById("content").innerHTML = await fetch(route).then((data) => data.text());
-    document.dispatchEvent(locationChangeEvent);
+  document.getElementById("content").innerHTML = await fetch(route).then(
+    (data) => data.text()
+  );
+  document.dispatchEvent(locationChangeEvent);
 }
 
 const goBack = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.go(-1);
-}
+  event = event || window.event;
+  event.preventDefault();
+  window.history.go(-1);
+};
 
 document.getElementById("back-arrow").addEventListener("click", goBack);
 
